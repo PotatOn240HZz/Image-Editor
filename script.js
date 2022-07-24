4fc380234b29932f8cb32f1d88e6b8ca10f87038
PotatOn240HZz <jo.yousefshaban@gmail.com>
2022-07-25 00:29:16 +0300

    Rotate and Flip Update

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
 function rangeSlide1(value) {
-    document.getElementById('rangeValue1').innerHTML = value;
+	document.getElementById('rangeValue1').innerHTML = value;
 }
 function rangeSlide2(value) {
-    document.getElementById('rangeValue2').innerHTML = value;
+	document.getElementById('rangeValue2').innerHTML = value;
 }
 function rangeSlide3(value) {
-    document.getElementById('rangeValue3').innerHTML = value;
+	document.getElementById('rangeValue3').innerHTML = value;
 }
 function rangeSlide4(value) {
-    document.getElementById('rangeValue4').innerHTML = value;
+	document.getElementById('rangeValue4').innerHTML = value;
 }
 function rangeSlide5(value) {
-    document.getElementById('rangeValue5').innerHTML = value;
+	document.getElementById('rangeValue5').innerHTML = value;
 }
 
 
-"use strict";
 
-//we need three functions here
-//1.dragNdrop(event)
+var rotate = 0;
+var scaleX = 1;
+var scaleY = 1;
+function f_rotate() {
+	rotate -= 90;
+	if (rotate == -360)
+		rotate = 0;
+	printRF();
+}
+function f_rotate_r() {
+	rotate += 90;
+	if (rotate == 360)
+		rotate = 0;
+	printRF();
+}
+function f_flip_h() {
+	if (scaleX == 1)
+		scaleX = -1;
+	else
+		scaleX = 1;
+	printRF();
+}
+function f_flip_v() {
+	if (scaleY == 1)
+		scaleY = -1;
+	else
+		scaleY = 1;
+	printRF();
+}
+function printRF() {
+	document.querySelector("#preview").style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+}
 
-function dragNdrop(event){
-    var filename = URL.createObjectURL(event.target.files[0]); //will store file name with src
-	var preview = document.getElementById('preview'); //the div where we will show image
-	var previewImg = document.createElement("img"); //creating new image tag
-	previewImg.setAttribute("src",filename); //setting attribute src of newly created "img" element to "filename"
+
+
+function dragNdrop(event) {
+	var filename = URL.createObjectURL(event.target.files[0]);
+	var preview = document.getElementById('preview');
+	var previewImg = document.createElement("img");
+	previewImg.setAttribute("src", filename);
 	preview.innerHTML = "";
-	preview.appendChild(previewImg); //finally append "img" element to parent element "preview"
+	preview.appendChild(previewImg);
+	document.getElementById('dragBox').style.display = 'none';
 }
-
-//2. drag
-
-function drag(){
-	//while dragging it will fire this function that will add the following class
+function drag() {
 	document.getElementById('uploadFile').parentNode.className = "draging dragBox";
 }
-
-//3. finally the drop()
-function drop(){
-    document.getElementById('dragBox').style.display = 'none';
+function drop() {
 	document.getElementById('uploadFile').parentNode.className = 'dragBox';
 }
-
-// we are done... 
