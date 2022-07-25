49654fb91074e46d61b48fc879ee039c632d5c54
PotatOn240HZz <jo.yousefshaban@gmail.com>
2022-07-25 17:49:18 +0300

    Width and height changers Update

--- script.js
+++ script.js
@@ -1,42 +1,8 @@
-var grayscale=0;
-var speia=0;
-var invert=0;
-var brightness=0
-var contrast=0;
-function printFF(){
-	document.querySelector("#preview").style.filter = "grayscale("+grayscale+"%) sepia("+speia+"%) invert("+invert*100+"%) brightness("+(parseInt(brightness)+100)+"%) contrast("+(parseInt(contrast)+100)+"%)";
-}
-function rangeSlide1(value) {
-	document.getElementById('rangeValue1').innerHTML = value;
-	grayscale=value;
-	printFF();
-}
-function rangeSlide2(value) {
-	document.getElementById('rangeValue2').innerHTML = value;
-	speia=value;
-	printFF();
-}
-function rangeSlide3(value) {
-	document.getElementById('rangeValue3').innerHTML = value;
-	invert=value;
-	printFF();
-}
-function rangeSlide4(value) {
-	document.getElementById('rangeValue4').innerHTML = value;
-	brightness=value;
-	printFF();
-}
-function rangeSlide5(value) {
-	document.getElementById('rangeValue5').innerHTML = value;
-	contrast=value;
-	printFF();
-}
-
-
-
 var rotate = 0;
 var scaleX = 1;
 var scaleY = 1;
+var tempwidth = 0;
+var tempheight = 0;
 function printRF() {
 	document.querySelector("#preview").style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
 }
@@ -44,12 +10,21 @@ function f_rotate() {
 	rotate -= 90;
 	if (rotate == -360)
 		rotate = 0;
+	if (rotate == -270 || rotate == -90)
+		document.getElementById("preview").firstElementChild.style.width = "746px";
+	else
+		document.getElementById("preview").firstElementChild.style.width = "" + tempwidth + "px";
 	printRF();
 }
 function f_rotate_r() {
 	rotate += 90;
 	if (rotate == 360)
 		rotate = 0;
+	if (rotate == 270 || rotate == 90)
+		document.getElementById("preview").firstElementChild.style.width = "746px";
+	else
+		document.getElementById("preview").firstElementChild.style.width = "" + tempwidth + "px";
+	console.log(tempheight);
 	printRF();
 }
 function f_flip_h() {
@@ -68,6 +43,50 @@ function f_flip_v() {
 }
 
 
+var grayscale = 0;
+var speia = 0;
+var invert = 0;
+var brightness = 0
+var contrast = 0;
+function printFF() {
+	document.querySelector("#preview").style.filter = "grayscale(" + grayscale + "%) sepia(" + speia + "%) invert(" + invert * 100 + "%) brightness(" + (parseInt(brightness) + 100) + "%) contrast(" + (parseInt(contrast) + 100) + "%)";
+}
+function rangeSlide1(value) {
+	document.getElementById('rangeValue1').innerHTML = value;
+	grayscale = value;
+	printFF();
+}
+function rangeSlide2(value) {
+	document.getElementById('rangeValue2').innerHTML = value;
+	speia = value;
+	printFF();
+}
+function rangeSlide3(value) {
+	document.getElementById('rangeValue3').innerHTML = value;
+	invert = value;
+	printFF();
+}
+function rangeSlide4(value) {
+	document.getElementById('rangeValue4').innerHTML = value;
+	brightness = value;
+	printFF();
+}
+function rangeSlide5(value) {
+	document.getElementById('rangeValue5').innerHTML = value;
+	contrast = value;
+	printFF();
+}
+
+function widthChange(value) {
+	if (document.getElementById("width").value.length == 0)
+		document.getElementById("preview").firstElementChild.style.width = "" + tempwidth + "px";
+	document.getElementById("preview").firstElementChild.style.width = "" + value + "px";
+}
+function heightChange(value) {
+	if (document.getElementById("height").value.length == 0)
+		document.getElementById("preview").firstElementChild.style.height = "" + tempheight + "px";
+	document.getElementById("preview").firstElementChild.style.height = "" + value + "px";
+}
 
 
 function dragNdrop(event) {
@@ -78,6 +97,14 @@ function dragNdrop(event) {
 	preview.innerHTML = "";
 	preview.appendChild(previewImg);
 	document.getElementById('dragBox').style.display = 'none';
+	previewImg.onload = function () {
+		if(previewImg.width <300)
+			previewImg.width=300;
+		if(previewImg.height < 300)
+			previewImg.height=300;
+		tempwidth=previewImg.width;
+		tempheight=previewImg.height;
+	}
 }
 function drag() {
 	document.getElementById('uploadFile').parentNode.className = "draging dragBox";
