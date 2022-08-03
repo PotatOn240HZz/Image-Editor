bdf2d13be45441d28bebc33c80365552f0b8f6dd
PotatOn240HZz <jo.yousefshaban@gmail.com>
2022-08-03 23:16:30 +0300

    Width/Height Textarea Update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var rotate = 0;
 var scaleX = 1;
 var scaleY = 1;
-var Iwidth = 0;
+var Iwidth = 0; //Original image width and height
 var Iheight = 0;
-var pwidth = 0;
+var uwidth = 0; //User modified image width and height
+var uheight = 0;
+var pwidth = 0; //Previewed image width and height
 var pheight = 0;
 
 function reset() {
+	/*--------------------------------------------------  resets all attributes -------------------------------------------------- */
 	rotate = 0, scaleX = 1, scaleY = 1, Iwidth = 0, Iheight = 0, pwidth = 0, pheight = 0;
 	grayscale = 0, speia = 0, invert = 0, brightness = 0, contrast = 0;
 	document.getElementById('rangeValue1').innerHTML = 0;
@@ -28,8 +31,9 @@ function reset() {
 	document.querySelector("#preview").style.filter = "grayscale(" + grayscale + "%) sepia(" + speia + "%) invert(" + invert * 100 + "%) brightness(" + (parseInt(brightness) + 100) + "%) contrast(" + (parseInt(contrast) + 100) + "%)";
 	document.querySelector("#preview").style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
 }
-function printRF() {
 
+function printRF() {
+	/*--------------------------------------------------  applies rotations and flips styles -------------------------------------------------- */
 	document.querySelector("#preview").firstElementChild.style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
 	document.getElementById('image-video').style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
 	videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
@@ -43,23 +47,21 @@ function printRF() {
 	first2Str = String(pheight).slice(0, 3);
 	first2Num = Number(first2Str);
 	document.getElementById("height").value = first2Str;
-	console.log(pheight);
-	console.log(pwidth);
 }
 function rotateR(rotate) {
+	/*--------------------------------------------------  applies rotations into images and their containers -------------------------------------------------- */
 	if (Math.abs(rotate) == 360)
-		this.rotate = 0;
+		rotate = 0;
+	this.rotate = rotate;
 	if (Math.abs(rotate) == 270 || Math.abs(rotate) == 90) {
 		if (pwidth > 700) {
 			pwidth = 700 * (pheight / pwidth);
 			pheight = 700;
-			console.log("problem1");
 		}
 		else {
 			var temp = pheight;
 			pheight = pwidth;
 			pwidth = temp;
-			console.log("problem2");
 		}
 		document.getElementById('image-video').style.height = pwidth;
 		document.getElementById('image-video').style.width = pheight;
@@ -67,13 +69,12 @@ function rotateR(rotate) {
 		document.getElementById('image1').style.width = pheight;
 	}
 	else {
-		pheight = Iheight;
-		pwidth = Iwidth;
+		pheight = uheight;
+		pwidth = uwidth;
 		document.getElementById('image-video').style.height = pheight;
 		document.getElementById('image-video').style.width = pwidth;
 		document.getElementById('image1').style.height = pheight;
 		document.getElementById('image1').style.width = pwidth;
-		console.log("problem3");
 	}
 	printRF();
 	var rect1 = videoContainer.getBoundingClientRect();
@@ -94,32 +95,48 @@ function rotateR(rotate) {
 }
 
 function f_rotate() {
-	rotate -= 90;
-	rotateR(rotate);
+	/*--------------------------------------------------  Rotate to the right -------------------------------------------------- */
+	if (document.getElementById('image1') == null)
+		alert("Please Upload A Picture");
+	else {
+		rotate += 90;
+		rotateR(rotate);
+	}
 }
 function f_rotate_r() {
-	rotate += 90;
-	rotateR(rotate);
+	/*--------------------------------------------------  Rotate to the left -------------------------------------------------- */
+	if (document.getElementById('image1') == null)
+		alert("Please Upload A Picture");
+	else {
+		rotate -= 90;
+		rotateR(rotate);
+	}
 }
 function f_flip_h() {
-	if (scaleX == 1)
-		scaleX = -1;
-	else
-		scaleX = 1;
-	console.log(scaleX + "," + scaleY);
-	document.querySelector("#preview").firstElementChild.style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
-	document.getElementById('image-video').style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+	/*--------------------------------------------------  Flip Horizontally -------------------------------------------------- */	if (document.getElementById('image1') == null)
+		alert("Please Upload A Picture");
+	else {
+		if (scaleX == 1)
+			scaleX = -1;
+		else
+			scaleX = 1;
+		document.querySelector("#preview").firstElementChild.style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+		document.getElementById('image-video').style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+	}
 }
 function f_flip_v() {
-	if (scaleY == 1)
-		scaleY = -1;
-	else
-		scaleY = 1;
-	console.log(scaleX + "," + scaleY);
-	document.querySelector("#preview").firstElementChild.style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
-	document.getElementById('image-video').style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+	/*--------------------------------------------------  Flip Vertically -------------------------------------------------- */
+	if (document.getElementById('image1') == null)
+		alert("Please Upload A Picture");
+	else {
+		if (scaleY == 1)
+			scaleY = -1;
+		else
+			scaleY = 1;
+		document.querySelector("#preview").firstElementChild.style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+		document.getElementById('image-video').style.transform = "scale(" + scaleX + "," + scaleY + ") rotate(" + (rotate) + "deg)";
+	}
 }
-
 
 var grayscale = 0;
 var speia = 0;
@@ -127,72 +144,183 @@ var invert = 0;
 var brightness = 0
 var contrast = 0;
 function printFF() {
+	/*--------------------------------------------------  applies styles -------------------------------------------------- */
 	document.querySelector("#preview").style.filter = "grayscale(" + grayscale + "%) sepia(" + speia + "%) invert(" + invert * 100 + "%) brightness(" + (parseInt(brightness) + 100) + "%) contrast(" + (parseInt(contrast) + 100) + "%)";
 }
 function rangeSlide1(value) {
-	document.getElementById('rangeValue1').innerHTML = value;
-	grayscale = value;
-	printFF();
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		document.getElementById('range1').value = 0;
+	}
+	else {
+		document.getElementById('rangeValue1').innerHTML = value;
+		grayscale = value;
+		printFF();
+	}
 }
 function rangeSlide2(value) {
-	document.getElementById('rangeValue2').innerHTML = value;
-	speia = value;
-	printFF();
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		document.getElementById('range2').value = 0;
+	}
+	else {
+		document.getElementById('rangeValue2').innerHTML = value;
+		speia = value;
+		printFF();
+	}
 }
 function rangeSlide3(value) {
-	document.getElementById('rangeValue3').innerHTML = value;
-	invert = value;
-	printFF();
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		document.getElementById('range3').value = 0;
+	}
+	else {
+		document.getElementById('rangeValue3').innerHTML = value;
+		invert = value;
+		printFF();
+	}
 }
 function rangeSlide4(value) {
-	document.getElementById('rangeValue4').innerHTML = value;
-	brightness = value;
-	printFF();
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		document.getElementById('range4').value = 0;
+	}
+	else {
+		document.getElementById('rangeValue4').innerHTML = value;
+		brightness = value;
+		printFF();
+	}
 }
 function rangeSlide5(value) {
-	document.getElementById('rangeValue5').innerHTML = value;
-	contrast = value;
-	printFF();
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		document.getElementById('range5').value = 0;
+	}
+	else {
+		document.getElementById('rangeValue5').innerHTML = value;
+		contrast = value;
+		printFF();
+	}
 }
 
 
+function isNumber(evt) {
+	/*--------------------------------------------------  Checks if width and Height attributes are only numbers -------------------------------------------------- */
+	if (document.getElementById('image1') == null) {
+		alert("Please Upload A Picture");
+		return false
+	}
+	evt = (evt) ? evt : window.event;
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+		return false;
+	}
+	else if (evt.keyCode == 13 && !evt.shiftKey) {
+		evt.preventDefault();
+	}
+	return true;
+}
+
+function FocusOut() {
+	document.getElementById("width").value = pwidth;
+	document.getElementById("height").value = pheight;
+}
 function widthChange(value) {
 	if (document.getElementById("width").value.length == 0 || document.getElementById("width").value == 0)
 		pwidth = Iwidth;
-	else if (document.getElementById("width").value > 1480) {
-		if (rotate == 0 || Math.abs(rotate) == 180)
-			pwidth = 1480;
-		else
-			pwidth = 700;
-	}
+	else if (document.getElementById("width").value > 1480 && (rotate == 0 || Math.abs(rotate) == 180))
+		pwidth = 1480;
+	else if (document.getElementById("width").value > 700 && (Math.abs(rotate) == 90 || Math.abs(rotate) == 270))
+		pwidth = 700;
 	else if (document.getElementById("width").value < 300)
 		pwidth = 300;
 	else
 		pwidth = value;
-	document.getElementById("preview").firstElementChild.style.width = "" + pwidth + "px";
-	document.getElementById("preview").firstElementChild.style.height = "" + pheight + "px";
-	videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+	if (Math.abs(rotate) == 270 || Math.abs(rotate) == 90) {
+		console.log("problem1!!!!!");
+		videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+		videoClipper.setAttribute("style", "width: 0px; height : " + pheight + "px;");
+		document.getElementById("preview").style.height = pheight;
+		document.getElementById("preview").style.width = pwidth;
+		document.getElementById('image1').style.height = pwidth;
+		document.getElementById('image1').style.width = pheight;
+		document.getElementById('image-video').style.height = pwidth;
+		document.getElementById('image-video').style.width = pheight;
+		rect1 = videoContainer.getBoundingClientRect();
+		rect2 = clippedVideo.getBoundingClientRect();
+		rect3 = document.querySelector("#preview").getBoundingClientRect();
+		document.querySelector("#preview").style.top = (rect1.top - rect3.top) + "px";
+		document.querySelector("#preview").style.left = (rect1.left - rect3.left) + "px";
+		clippedVideo.style.top = (rect1.top - rect2.top) + "px";
+		clippedVideo.style.left = (rect1.left - rect2.left) + "px";
+	}
+	else {
+		console.log("problem2!!!!!");
+		videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+		videoClipper.setAttribute("style", "width: 0px; height : " + pheight + "px;");
+		document.getElementById("preview").style.height = pheight;
+		document.getElementById("preview").style.width = pwidth;
+		document.getElementById('image1').style.height = pheight;
+		document.getElementById('image1').style.width = pwidth;
+		document.getElementById('image-video').style.height = pheight;
+		document.getElementById('image-video').style.width = pwidth;
+		rect1 = videoContainer.getBoundingClientRect();
+		rect2 = clippedVideo.getBoundingClientRect();
+		rect3 = document.querySelector("#preview").getBoundingClientRect();
+		document.querySelector("#preview").style.top = 0 + "px";
+		document.querySelector("#preview").style.left = 0 + "px";
+		clippedVideo.style.top = 0 + "px";
+		clippedVideo.style.left = 0 + "px";
+	}
+	uwidth = pwidth;
+	uheight = pheight;
 }
+
 function heightChange(value) {
 	if (document.getElementById("height").value.length == 0 || document.getElementById("height").value == 0)
 		pheight = Iheight;
-	else if (document.getElementById("height").value > 700) {
-		if (rotate == 0 || Math.abs(rotate) == 180)
-			pheight = 700;
-		else
-			pheight = 1480;
-	}
+	else if (document.getElementById("height").value > 700 && (rotate == 0 || Math.abs(rotate) == 180))
+		pheight = 700;
+	else if (document.getElementById("height").value > 1480 && (Math.abs(rotate) == 90 || Math.abs(rotate) == 270))
+		pheight = 1480;
 	else if (document.getElementById("height").value < 300)
 		pheight = 300;
 	else
 		pheight = value;
-	document.getElementById("preview").firstElementChild.style.height = "" + pheight + "px";
-	document.getElementById("preview").firstElementChild.style.width = "" + pwidth + "px";
-	videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+	rect1 = videoContainer.getBoundingClientRect();
+	rect2 = clippedVideo.getBoundingClientRect();
+	rect3 = document.querySelector("#preview").firstElementChild.getBoundingClientRect();
+	if (Math.abs(rotate) == 270 || Math.abs(rotate) == 90) {
+		videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+		videoClipper.setAttribute("style", "width: 0px; height : " + pheight + "px;");
+		document.getElementById("preview").style.height = pheight;
+		document.getElementById("preview").style.width = pwidth;
+		document.getElementById('image1').style.height = pwidth;
+		document.getElementById('image1').style.width = pheight;
+		document.getElementById('image-video').style.height = pwidth;
+		document.getElementById('image-video').style.width = pheight;
+		document.querySelector("#preview").style.top = (rect1.top - rect3.top) + "px";
+		document.querySelector("#preview").style.left = (rect1.left - rect3.left) + "px";
+		clippedVideo.style.top = (rect1.top - rect2.top) + "px";
+		clippedVideo.style.left = (rect1.left - rect2.left) + "px";
+	}
+	else {
+		videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
+		videoClipper.setAttribute("style", "width: 0px; height : " + pheight + "px;");
+		document.getElementById("preview").style.height = pheight;
+		document.getElementById("preview").style.width = pwidth;
+		document.getElementById('image1').style.height = pheight;
+		document.getElementById('image1').style.width = pwidth;
+		document.getElementById('image-video').style.height = pheight;
+		document.getElementById('image-video').style.width = pwidth;
+		document.querySelector("#preview").style.top = 0 + "px";
+		document.querySelector("#preview").style.left = 0 + "px";
+		clippedVideo.style.top = 0 + "px";
+		clippedVideo.style.left = 0 + "px";
+	}
 }
 
-
-
+/*--------------------------------------------------  Uploading An Image -------------------------------------------------- */
 var videoContainer,
 	videoClipper,
 	clippedVideo;
@@ -228,6 +356,8 @@ function dragNdrop(event) {
 		pwidth = Iwidth;
 		Iheight = previewImg.offsetHeight;
 		pheight = Iheight;
+		uwidth = pwidth;
+		uheight = pheight;
 		document.getElementById("preview").style.width = "" + pwidth + "px";
 		document.getElementById("preview").style.height = "" + pheight + "px";
 		videoContainer.setAttribute("style", "width:" + pwidth + "px; height : " + pheight + "px;");
@@ -243,8 +373,6 @@ function dragNdrop(event) {
 		document.querySelector("#preview").style.left = (rect1.left - rect3.left) + "px";
 		clippedVideo.style.top = (rect1.top - rect2.top) + "px";
 		clippedVideo.style.left = (rect1.left - rect2.left) + "px";
-		console.log(pheight);
-		console.log(pwidth);
 	}
 }
 function drag() {
@@ -255,7 +383,7 @@ function drop() {
 	document.getElementById('uploadFile').parentNode.className = 'dragBox';
 }
 
-
+/*--------------------------------------------------  Downloading An Image -------------------------------------------------- */
 function canvasf(el) {
 	var canvas = document.getElementById('myCanvas');
 	var ctx = canvas.getContext('2d');
@@ -306,7 +434,7 @@ function canvasf(el) {
 	el.href = dt;
 };
 
-
+/*--------------------------------------------------  Tracking the mouse to change old image preview width -------------------------------------------------- */
 function trackLocation(e) {
 	var rect = videoContainer.getBoundingClientRect(),
 		position = ((e.pageX - rect.left) / videoContainer.offsetWidth) * 100;
@@ -324,5 +452,4 @@ function trackLocation(e) {
 
 function leaveLocation(e) {
 	videoClipper.style.width = "0px";
-	console.log("asdasd");
 }
